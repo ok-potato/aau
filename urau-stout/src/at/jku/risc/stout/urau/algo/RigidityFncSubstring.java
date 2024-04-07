b92e88d30062286cf9b09402684a82c5cdf6e608
Daniel Sunthimer <d.sunthimer@gmail.com>
2024-04-07 18:44:30 +0200

    ctr+alt+L

--- urau-stout/src/at/jku/risc/stout/urau/algo/RigidityFncSubstring.java
+++ urau-stout/src/at/jku/risc/stout/urau/algo/RigidityFncSubstring.java
@@ -33,7 +33,7 @@ import at.jku.risc.stout.urau.data.atom.TermAtom;
  * O(max(m1, ..., mk, n1, ..., nk))<br>
  * <br>
  * Possible optimization:
- * 
+ *
  * <pre>
  *     trim off the matching items at the beginning
  *     while start <= m_end and start <= n_end and X[start] = Y[start]
@@ -43,84 +43,87 @@ import at.jku.risc.stout.urau.data.atom.TermAtom;
  *         m_end := m_end - 1
  *         n_end := n_end - 1
  * </pre>
- * 
+ *
  * @author Alexander Baumgartner
  */
 public class RigidityFncSubstring extends RigidityFnc {
-	private int minLen;
-	private int traceLen = 32;
-	// 4 * max(m, n) ~ O(max(m, n))
-	private int[][] matchTrace = new int[4][traceLen];
-
-	@Override
-	public RigidityFnc setMinLen(int minLen) {
-		if (minLen < 0)
-			throw new IllegalArgumentException("minLen cannot be < 0");
-		this.minLen = minLen;
-		return this;
-	}
-
-	@Override
-	public AlignmentList compute(TermAtomList left, TermAtomList right) {
-		int lenL = left.size(), lenR = right.size();
-		int matchLen = minLen;
-		AlignmentList alignList = AlignmentList.obtainList();
-		if (matchLen > lenL || matchLen > lenR)
-			return alignList;
-		int matchCount = 0;
-		// check size of recycled memory space
-		if (traceLen < lenL || traceLen < lenR) {
-			traceLen = Math.max(lenL, lenR);
-			matchTrace = new int[4][traceLen];
-		}
-		int[] traceMaxI = matchTrace[0];
-		int[] traceMaxJ = matchTrace[1];
-		int[] traceCurr = matchTrace[2];
-		int[] tracePrev = matchTrace[3];
-
-		// find longest common substrings
-		for (int i = 0; i < lenL; i++) {
-			TermAtom tLeft = left.get(i);
-			for (int j = 0; j < lenR; j++) {
-				int currLen;
-				if (tLeft.equals(right.get(j))) {
-					if (i * j == 0) {
-						currLen = 1;
-					} else {
-						currLen = tracePrev[j - 1] + 1;
-					}
-					if (currLen >= matchLen) {
-						if (currLen > matchLen) {
-							matchLen = currLen;
-							matchCount = 0;
-						}
-						traceMaxI[matchCount] = i;
-						traceMaxJ[matchCount] = j;
-						matchCount++;
-					}
-				} else {
-					currLen = 0;
-				}
-				traceCurr[j] = currLen;
-			}
-			int[] swap = traceCurr;
-			traceCurr = tracePrev;
-			tracePrev = swap;
-		}
-
-		if (matchLen == 0) {
-			alignList.nextAlignment();
-			return alignList;
-		}
-
-		// create result set of alignments
-		for (int i = 0; i < matchCount; i++) {
-			Alignment align = alignList.nextAlignment();
-			int n = traceMaxI[i] + 1;
-			int rIdx = traceMaxJ[i] - matchLen + 1;
-			for (int lIdx = n - matchLen; lIdx < n; lIdx++, rIdx++)
-				align.addAtom(left.get(lIdx), lIdx, rIdx);
-		}
-		return alignList;
-	}
+    private int minLen;
+    private int traceLen = 32;
+    // 4 * max(m, n) ~ O(max(m, n))
+    private int[][] matchTrace = new int[4][traceLen];
+    
+    @Override
+    public RigidityFnc setMinLen(int minLen) {
+        if (minLen < 0) {
+            throw new IllegalArgumentException("minLen cannot be < 0");
+        }
+        this.minLen = minLen;
+        return this;
+    }
+    
+    @Override
+    public AlignmentList compute(TermAtomList left, TermAtomList right) {
+        int lenL = left.size(), lenR = right.size();
+        int matchLen = minLen;
+        AlignmentList alignList = AlignmentList.obtainList();
+        if (matchLen > lenL || matchLen > lenR) {
+            return alignList;
+        }
+        int matchCount = 0;
+        // check size of recycled memory space
+        if (traceLen < lenL || traceLen < lenR) {
+            traceLen = Math.max(lenL, lenR);
+            matchTrace = new int[4][traceLen];
+        }
+        int[] traceMaxI = matchTrace[0];
+        int[] traceMaxJ = matchTrace[1];
+        int[] traceCurr = matchTrace[2];
+        int[] tracePrev = matchTrace[3];
+        
+        // find longest common substrings
+        for (int i = 0; i < lenL; i++) {
+            TermAtom tLeft = left.get(i);
+            for (int j = 0; j < lenR; j++) {
+                int currLen;
+                if (tLeft.equals(right.get(j))) {
+                    if (i * j == 0) {
+                        currLen = 1;
+                    } else {
+                        currLen = tracePrev[j - 1] + 1;
+                    }
+                    if (currLen >= matchLen) {
+                        if (currLen > matchLen) {
+                            matchLen = currLen;
+                            matchCount = 0;
+                        }
+                        traceMaxI[matchCount] = i;
+                        traceMaxJ[matchCount] = j;
+                        matchCount++;
+                    }
+                } else {
+                    currLen = 0;
+                }
+                traceCurr[j] = currLen;
+            }
+            int[] swap = traceCurr;
+            traceCurr = tracePrev;
+            tracePrev = swap;
+        }
+        
+        if (matchLen == 0) {
+            alignList.nextAlignment();
+            return alignList;
+        }
+        
+        // create result set of alignments
+        for (int i = 0; i < matchCount; i++) {
+            Alignment align = alignList.nextAlignment();
+            int n = traceMaxI[i] + 1;
+            int rIdx = traceMaxJ[i] - matchLen + 1;
+            for (int lIdx = n - matchLen; lIdx < n; lIdx++, rIdx++) {
+                align.addAtom(left.get(lIdx), lIdx, rIdx);
+            }
+        }
+        return alignList;
+    }
 }
